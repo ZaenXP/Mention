f36bfabca9328257547c213228f3312e2cb99c86
ZaenXp <107419846+ZaenXP@users.noreply.github.com>
2022-06-17 13:41:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ async def start(event):
     link_preview=False,
     buttons=(
       [
-        Button.url("➕ [ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴜᴘ] ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
-      ], 
+        Button.url('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿𝚂 ➕ ', 'https://t.me/MEMBER_TAGERBOT?startgroup=true'), 
+      ]
         Button.url('[Channel]', 'https://t.me/GabutnyaZaen'),
         Button.url('[Dev]', 'https://t.me/Mafia_TobaTz')
     )
